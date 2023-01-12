bee76c72c8500d3d0af34f3bc1d3aafbce7339db
LINSANITY03 <pujantamang92@gmail.com>
2023-01-12 19:53:36 +0545

    first commit

---
+++ search_practice/app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Products(models.Model):
+
+    class Meta:
+        db_table = 'Products'
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date_uploaded = models.DateField()
+    time_uploaded = models.TimeField()
+
+    def __str__(self) -> str:
+        return self.title
