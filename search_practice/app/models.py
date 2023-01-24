00696f2b78ddb9becc24f407d2f0f1e7989226ae
govinde kumar <govindekumar99@gmail.com>
2023-01-24 18:33:08 +0545

    added tests --elasticsearch

--- search_practice/app/models.py
+++ search_practice/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 class Products(models.Model):
@@ -10,8 +11,8 @@ class Products(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    date_uploaded = models.DateField()
-    time_uploaded = models.TimeField()
+    date_uploaded = models.DateField(default=datetime.datetime.now())
+    time_uploaded = models.TimeField(default=datetime.datetime.now())
 
     def __str__(self) -> str:
         return self.title
